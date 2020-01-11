eff88ea68ff7dd63ed61ea1d3c537116e7e6ca4f
MartyGer <34271913+MartyGer@users.noreply.github.com>
2020-01-11 12:42:29 +0530

    Update register.php

--- Login/register.php
+++ Login/register.php
@@ -1,8 +1,8 @@
 <?php
 // Change this to your connection info.
-$DATABASE_HOST = 'sql212.byetcluster.com';
+$DATABASE_HOST = 'sql212.freecluster.eu';
 $DATABASE_USER = 'epiz_25041824';
-$DATABASE_PASS = 'maia!234maia!234';
+$DATABASE_PASS = 'XEXxRwaCxjJDO';
 $DATABASE_NAME = 'epiz_25041824_test';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
